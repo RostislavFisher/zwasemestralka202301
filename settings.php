b5f21b49e24be2fb2d65d2cbe416f70f277a89df
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2023-09-29 19:40:00 +0300

    settings.php -> value of Rooms are added serve.php -> index.html added fetching data

--- settings.php
+++ settings.php
@@ -33,19 +33,19 @@ $KitchenModule = new RoomTemperatureModule();
 $KitchenModule->name = "Kitchen";
 $KitchenModule->description = "This is the kitchen";
 $KitchenModule->icon = "icon";
-$KitchenModule->value = "value";
+$KitchenModule->value = "24";
 
 $BedroomModule = new RoomTemperatureModule();
 $BedroomModule->name = "Bedroom";
 $BedroomModule->description = "This is the bedroom";
 $BedroomModule->icon = "icon";
-$BedroomModule->value = "value";
+$BedroomModule->value = "22";
 
 $BathroomModule = new RoomTemperatureModule();
 $BathroomModule->name = "Bathroom";
 $BathroomModule->description = "This is the bathroom";
 $BathroomModule->icon = "icon";
-$BathroomModule->value = "value";
+$BathroomModule->value = "20";
 
 $AllModules = new Modules();
 $AllModules->addModule($KitchenModule);
