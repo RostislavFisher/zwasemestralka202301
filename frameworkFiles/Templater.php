b772d5010e2fec4013022ebe5ce5de9544eedb99
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2023-10-04 09:13:42 +0300

    Templater.php -> bug fixed with echo + variables null

--- frameworkFiles/Templater.php
+++ frameworkFiles/Templater.php
@@ -3,7 +3,7 @@
 class Templater
 {
     public $file;
-    public $variables;
+    public $variables = [];
 
     public function __construct($file){
         $this->file = $file;
@@ -14,8 +14,6 @@ class Templater
 //        $content = file_get_contents($this->file);
         include $this->file;
         $content = ob_get_clean();
-        echo $content;
-
 
         foreach ($this->variables as $key => $value) {
             $content = str_replace("{{".$key."}}", $value, $content);
