93a5deb426052e6dd07770b193dd583a7c8b2243
Rostyslav <rostislavfisher@gmail.com>
2023-12-06 17:42:56 +0100

    Login.js -> created Login.html -> js code has been moved to Login.js

---
+++ js/Login.js
@@ -0,0 +1,29 @@
+function Login(){
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    let req = new XMLHttpRequest();
+    let formData = new FormData();
+    formData.append("name", name);
+    formData.append("password", password);
+    req.open("POST", '/userLogin');
+    req.send(formData);
+    req.onreadystatechange = function () {
+        if (req.readyState === 4 && req.status === 200) {
+            let response = JSON.parse(req.responseText);
+            if (response.result === "OK"){
+                window.location.href = "/smarthouse/index.html";
+            }
+            else{
+                alert("Wrong name or password");
+            }
+        }
+    }
+
+}
+
+let LoginForm = document.getElementById("LoginForm");
+LoginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Login();
+
+})
