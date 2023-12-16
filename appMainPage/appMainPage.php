22cde2f0d107c76033a89ffa4cf54664ce421b2a
Rostyslav <rostislavfisher@gmail.com>
2023-12-16 19:20:40 +0100

    appMainPage.php -> comments have been added

--- appMainPage/appMainPage.php
+++ appMainPage/appMainPage.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * appMainPage is an implementation of WebEntityCustom
+ */
 class appMainPage extends WebEntityCustom
 {
 
+    /**
+     * returns the html code of the page
+     */
     public function __toString(){
         return "<html><body>hello</body></html>";
     }
+
+    /**
+     * Executes the web entity
+     * @param $listOfAllObjects: the data
+     */
     function execute($listOfAllObjects)
     {
         return "<html><body>hello</body></html>";
