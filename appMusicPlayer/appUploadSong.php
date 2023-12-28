7100d080b44daee34dffc8cfd8fd819d65d79367
Rostyslav <rostislavfisher@gmail.com>
2023-12-28 03:49:38 +0100

    appUploadSong.php -> regexes added to check name

--- appMusicPlayer/appUploadSong.php
+++ appMusicPlayer/appUploadSong.php
@@ -19,7 +19,15 @@ class appUploadSong extends WebEntityCustom
         // Access the file data and non-file field
         $fileData = $formData['song']['data'];
         $fileData = str_replace(' ', '+', $fileData);
+        $response = new HTTPResponse();
         $nonFileField = $formData['songName'];
+
+        if (!(preg_match('/^[a-zA-Z0-9_]+/', $nonFileField))) {
+            $response->status = 400;
+            echo "Invalid song name";
+            $response->body = json_encode(array("result" => "Error", "comment" => "Invalid song name",), JSON_PRETTY_PRINT);
+            return $response;
+        }
         file_put_contents("songs/" . $nonFileField . ".mp3", $formData['song']['data']);
         $response = new HTTPResponse();
         $response->body = json_encode($formData, JSON_PRETTY_PRINT);
